90b055ba7e7a4c83b8ed0edebcefdce81dc3f848
arai <niimaru09@gmail.com>
2021-10-16 01:17:53 +0900

    出発地と到着地を入力してURLを生成し、そのURLのhtmlをパースして出発時刻を取得する

--- trainApp/ViewController.swift
+++ trainApp/ViewController.swift
@@ -9,11 +9,6 @@ import UIKit
 import Alamofire
 import Kanna
 
-
-
-// https://api.ekispert.jp/v1/json/search/course/light?key=LE_2McsphRXduecu&from=磯子&to=横浜
-// 駅名検索のやつでプルダウンにして、そこから選択する形だったらいいね。
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var debugButton: UIButton!
@@ -22,7 +17,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        fetchTrainDataFromUrl
         debugButton.addTarget(self, action: #selector(debugButtonPressed(_:)), for: .touchUpInside)
     }
     
@@ -33,37 +27,30 @@ class ViewController: UIViewController {
         let parameters = ["key": Secrets.accessKey, "from": "磯子", "to": "東京"]
         
         AF.request(baseUrl + path, method: .get, parameters: parameters, headers: [:])
-            .validate()
-            .responseJSON { response in
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-//                jsonDecoder.dateDecodingStrategy = .iso8601
-                guard let data = response.data else { return }
-                if response.error != nil {
-                    print(response.error.debugDescription)
-                } else {
-                    do {
-                        print("あ")
-                        print(response)
-                        print(data)
-                        let result = try jsonDecoder.decode(TrainSearchResult.self, from: data)
-                        print(result.ResultSet.ResourceURI)
-                    } catch {
-                        print("い")
-                        print(error)
-                    }
-                    
-//                    self.fetchTrainDataFromUrl()
+        .validate()
+        .responseJSON { response in
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            jsonDecoder.dateDecodingStrategy = .iso8601
+            guard let data = response.data else { return }
+            if response.error != nil {
+                print(response.error.debugDescription)
+            } else {
+                do {
+                    let result = try jsonDecoder.decode(TrainSearchResult.self, from: data)
+                    guard let resourceURI = result.ResultSet.ResourceURI else { return }
+                    self.fetchTrainDataFromUrl(resourceURI)
+                } catch {
+                    print(error)
                 }
             }
+        }
         
         
     }
     
-    func fetchTrainDataFromUrl() {
-        
-        let url = "https://roote.ekispert.net/ja/result?arr=%E6%A8%AA%E6%B5%9C&arr_code=23368&connect=true&day=12&dep=%E7%A3%AF%E5%AD%90&dep_code=23073&express=true&highway=true&hour=6&liner=true&local=true&locale=ja&minute10=4&minute1=9&plane=true&provider=&shinkansen=true&ship=true&sort=time&surcharge=3&ticket_type=0&transfer=2&type=dep&utf8=%E2%9C%93&via1=&via1_code=&via2=&via2_code=&yyyymm=202110"
-        
+    func fetchTrainDataFromUrl(_ url: String) {
+                
         AF.request(url).responseString { response in
 
             switch response.result {
@@ -71,7 +58,6 @@ class ViewController: UIViewController {
 
                 if let doc = try? HTML(html: value, encoding: .utf8) {
                     
-
                     for value in doc.xpath("//span[@class='dep_date']") {
                         print(value.text)
                     }
